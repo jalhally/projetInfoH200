0732623755cd56b7a782cf4f743ac59a42453bf6
jalhally <jobrann@hotmail.com>
2014-04-10 03:40:59 +0200

    Me suis fait baiser hihihi mais j'ai retapé les classes. Je gagne encore des lignes bande de noobs hihi. Javier y a une erreur dans ton monster, quand tu bouge à droite/gauche c'est setXpos qui change et pas setYpos noob.
    Signed-off-by: jalhally <jobrann@hotmail.com>

--- SpawnerMonster.java
+++ SpawnerMonster.java
@@ -1,4 +1,13 @@
 
-public class SpawnerMonster {
+public class SpawnerMonster extends Decor {
+	
+	public SpawnerMonster(int xPos, int yPos, String name) {
+		super(xPos,yPos,name);
+	}
+	
+	public void spawnMonster() {
+		
+	}
+	
 
 }
