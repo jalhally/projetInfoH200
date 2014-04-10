ca622601956090c1916b4b42378b39a5d60d6093
Fernando Perez <fperezfe@ulb.ac.be>
2014-04-10 02:15:23 +0200

    Signed-off-by: Fernando Perez <fperezfe@ulb.ac.be>
    meh

--- Monster.java
+++ Monster.java
@@ -1,4 +1,35 @@
 
-public class Monster {
+public class Monster extends Character {
 
+	public Monster (int lifePoint, int xPos, int yPos, int speed,int direction)
+	{
+		super (lifePoint, xPos, yPos, speed, direction);
+	}
+	
+	public void moveUp ()
+	{
+		setYPos(yPos + 1);
+	}
+
+	public void moveDown ()
+	{
+		setYPos(yPos - 1);
+	}
+
+	public void moveRight ()
+	{
+		setYPos(xPos + 1);
+	}
+
+	public void moveLeft ()
+	{
+		setYPos(xPos - 1);
+	}
+	
+	public void dealDamage(Link Link)
+	{
+		Link.setLifePoint(Link.getLifePoint() - 1);
+	}
+	
+	//IA
 }
