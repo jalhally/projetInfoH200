c7ba0daaa60db9352362959edac6a4828a0b4598
Bakacross <nintendo_corp@msn.com>
2014-04-10 22:32:40 +0200

    Signed-off-by: Bakacross <nintendo_corp@msn.com>

--- Projectile.java
+++ Projectile.java
@@ -1,10 +1,42 @@
 
 public class Projectile extends Damage {
-	public Projectile(int xPos, int yPos,String name,int damage){
+	private int direction;
+	public Projectile(int xPos, int yPos,String name, int damage, int direction){
 		super(xPos,yPos,name,damage);
+		this.direction = direction;
 	}
 	
-	public void move(){
-		
+	public int getDirection()
+	{ 
+		return direction;
+	}
+	
+	public void setDirection (int direction)
+	{
+		this.direction = direction;
+	}
+	
+	public void moveUp ()
+	{
+		setYPos(getYPos() + 1);
+		this.direction = 1;
+	}
+
+	public void moveDown ()
+	{
+		setYPos(getYPos() - 1);
+		this.direction = 2;
+	}
+
+	public void moveRight ()
+	{
+		setXPos(getXPos() + 1);
+		this.direction = 3;
+	}
+
+	public void moveLeft ()
+	{
+		setXPos(getXPos() - 1);
+		this.direction = 4;
 	}
 }
